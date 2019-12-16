45aa870d350337e93051142406327b4af83720e5
Slav Donchev <slav.donchev@gmail.com>
2019-12-16 09:30:22 +0200

    add packer ptoject

---
+++ scripts/provision.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Installs Docker Engine - Community
+
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# install the latest version of the Docker Engine - Comunity
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
